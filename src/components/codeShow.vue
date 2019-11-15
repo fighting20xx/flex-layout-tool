872932cc9d1a38d66f87029a76d37fd2da5d9d90
seven li <liwy@mingdutech.com>
2019-11-15 17:26:54 +0800

    123

--- src/components/codeShow.vue
+++ src/components/codeShow.vue
@@ -2,20 +2,7 @@
     <div class="codeShow-top">
         <div>
                 <h3>父容器CSS:</h3>
-                <pre class="code-show-pre">
-.items {
-    display: flex;
-    <span v-show="isShowflexDirection">flex-direction: {{ fatherOption.flexDirection }};</span>
-    <span v-show="isShowflexWrap">flex-wrap: {{ fatherOption.flexWrap }};</span>
-    <span v-show="isShowjustifyContent">justify-content: {{ fatherOption.justifyContent }};</span>
-    <span v-show="isShowalignItems">align-items: {{ fatherOption.alignItems }};</span>
-    <span v-show="isShowalignContent">align-content: {{ fatherOption.alignContent }};</span>
-    <span v-show="isShowmargin">align-content: {{ fatherOption.alignContent }};</span>
-    <span v-show="isShowpading">align-content: {{ fatherOption.alignContent }};</span>
-
- };
-
-                </pre>
+                <pre v-html="fatherCodeText">     </pre>
         </div>
         <el-row>
             <el-col :span="24">
@@ -26,6 +13,7 @@
     flex: {{ item.flexGrow }} {{ item.flexShrink }} {{ item.flexBasis }};
     order: {{ item.order }};
 }
+
                 </pre>
             </el-col>
         </el-row>
@@ -45,29 +33,137 @@
 			}
 		},
         computed:{
-			isShowflexDirection:function () {
-                return 'row' !== this.fatherOption.flexDirection;
+
+            fatherCodeText:function () {
+				let self = this;
+				let positionIndex  = 0;
+				let str ='';
+				str += splitCode(positionIndex,'.flex-contenter{')
+				str += splitCode(positionIndex+1,'display: flex;')
+
+				if(this.isShowflexDirection){
+					str += splitCode(positionIndex+1,`flex-direction: ${self.fatherOption.flexDirection };`)
+				}
+				if(this.isShowflexWrap){
+					str += splitCode(positionIndex+1,`flex-wrap: ${self.fatherOption.flexWrap };`)
+				}
+				if(this.isShowjustifyContent){
+					str += splitCode(positionIndex+1,`justify-content: ${self.fatherOption.justifyContent };`)
+				}
+				if(this.isShowalignItems){
+					str += splitCode(positionIndex+1,`align-items: ${self.fatherOption.alignItems };`)
+				}
+				if(this.isShowalignContent){
+					str += splitCode(positionIndex+1,`align-content:  ${self.fatherOption.alignContent };`)
+				}
+				if(this.isShowmargin){
+					str += splitCode(positionIndex+1,`margin: ${self.fatherOption.margin }px;`)
+				}
+				if(this.isShowpading){
+					str += splitCode(positionIndex+1,`padding: ${self.fatherOption.padding }px;`)
+				}
+
+
+
+				str += self.childrenCodeText;
+				str += splitCode(positionIndex,'}');
+                return str
 			},
-            isShowflexWrap:function () {
-                return 'nowrap' !== this.fatherOption.flexWrap;
-			},
-            isShowjustifyContent:function () {
-                return 'flex-start' !== this.fatherOption.justifyContent;
-			},
-            isShowalignItems:function () {
-                return 'stretch' !== this.fatherOption.alignItems;
-			},
-            isShowalignContent:function () {
-                return 'stretch' !== this.fatherOption.alignContent;
-			},
-            isShowmargin:function () {
-                return  this.fatherOption.margin >0;
-			},
-            isShowpading:function () {
-                return   this.fatherOption.padding > 0;
-			},
+
+            childrenCodeText:function () {
+				let self = this;
+				let positionIndex  = 1;
+				let str ='';
+				str += splitCode(positionIndex,'');
+				str += splitCode(positionIndex,'.item-common{');
+				switch (self.childOption.widthType) {
+					case "flex":
+						str += splitCode(positionIndex+1,'flex: 1;');
+						break;
+					case "percent":
+						str += splitCode(positionIndex+1,`width: ${self.childOption.itemWidth } + %;`);
+						break;
+					case "value":
+						str += splitCode(positionIndex+1,`width: ${self.childOption.itemWidth } + px;`);
+						break;
+					default:
+						1===1;
+				}
+				switch (self.childOption.heightType) {
+					case "flex":
+						str += splitCode(positionIndex+1,'flex: 1;');
+						break;
+					case "percent":
+						str += splitCode(positionIndex+1,`height: ${self.childOption.itemHeight } + %;`);
+						break;
+					case "value":
+						str += splitCode(positionIndex+1,`height: ${self.childOption.itemHeight } + px;`);
+						break;
+					default:
+						1===1;
+				}
+
+
+
+				if(this.isShowflexDirection){
+					str += splitCode(positionIndex+1,`flex-direction: ${self.fatherOption.flexDirection };`)
+				}
+				if(this.isShowflexWrap){
+					str += splitCode(positionIndex+1,`flex-wrap: ${self.fatherOption.flexWrap };`)
+				}
+				if(this.isShowjustifyContent){
+					str += splitCode(positionIndex+1,`justify-content: ${self.fatherOption.justifyContent };`)
+				}
+				if(this.isShowalignItems){
+					str += splitCode(positionIndex+1,`align-items: ${self.fatherOption.alignItems };`)
+				}
+				if(this.isShowalignContent){
+					str += splitCode(positionIndex+1,`align-content:  ${self.fatherOption.alignContent };`)
+				}
+				if(this.isShowmargin){
+					str += splitCode(positionIndex+1,`margin: ${self.fatherOption.margin }px;`)
+				}
+				if(this.isShowpading){
+					str += splitCode(positionIndex+1,`padding: ${self.fatherOption.padding }px;`)
+				}
+
+				str += splitCode(positionIndex,'}');
+				str += splitCode(positionIndex,'');
+
+				this.itemLists.forEach(function (item,index) {
+					str += splitCode(positionIndex,`.item-${index}{`);
+
+					if(self.isShowChildMargin){
+						str += splitCode(positionIndex+1,`margin: ${self.childOption.margin };`)
+					}
+					if(self.isShowChildPadding){
+						str += splitCode(positionIndex+1,`padding: ${self.childOption.padding };`)
+					}
+					str += splitCode(positionIndex+1,`margin: ${self.fatherOption.margin };`);
+				// .item-{{ index }} {
+				// 		align-self: {{ item.alignSelf }};
+				// 		flex: {{ item.flexGrow }} {{ item.flexShrink }} {{ item.flexBasis }};
+				// 		order: {{ item.order }};
+
+
+
+						str += splitCode(positionIndex,'}');
+				});
+
+				return str
+			}
         }
 	}
+
+	function splitCode(postion = 0,str) {
+        let s = '';
+        for(var i=0 ;i<postion;i++){
+			s += '   '
+        }
+		s = s + str +'\r\n';
+        return  s;
+	}
+
 </script>
 
 <style lang="less">
--- src/mixins/vuexOptions.js
+++ src/mixins/vuexOptions.js
@@ -30,6 +30,43 @@ const vuexOptionsMixin ={
 				this.$store.commit('itemLists',v);
 			},
 		},
+
+
+		/**
+		 * 父容器，是否显示属性值
+		 * */
+		isShowflexDirection:function () {
+			return 'row' !== this.fatherOption.flexDirection;
+		},
+		isShowflexWrap:function () {
+			return 'nowrap' !== this.fatherOption.flexWrap;
+		},
+		isShowjustifyContent:function () {
+			return 'flex-start' !== this.fatherOption.justifyContent;
+		},
+		isShowalignItems:function () {
+			return 'stretch' !== this.fatherOption.alignItems;
+		},
+		isShowalignContent:function () {
+			return 'stretch' !== this.fatherOption.alignContent;
+		},
+		isShowmargin:function () {
+			return  this.fatherOption.margin !== 0;
+		},
+		isShowpading:function () {
+			return   this.fatherOption.padding !==0;
+		},
+
+
+		/**
+		 * 子项目，是否显示属性值
+		 * */
+		isShowChildMargin:function () {
+			return this.childOption.margin >0;
+		},
+		isShowChildPadding:function () {
+			return this.childOption.padding >0;
+		},
 	}
 };
 export default vuexOptionsMixin
